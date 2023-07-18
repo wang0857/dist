d01ae28ae38cc3b0221a5e6d1d5b153cddab85ee
Ruo-Fang Wang <wang0857@algonquinlive.com>
2023-07-17 20:52:54 -0400

    first commit

---
+++ _nuxt/about.ea5553d6.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./entry.0b813844.js";const o={};function r(a,n){return c(),t("h2",null,"Here is my about virtual page.")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ _nuxt/index.df7a3d48.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.0b813844.js";const r={};function t(o,a){return n(),c("h2",null,"Here is my index virtual page.")}const _=e(r,[["render",t]]);export{_ as default};
